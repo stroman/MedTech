53a942d099eee43c3a276600632296cfb0366a3d
stroman <stefmanB33@gmail.com>
2014-10-15 19:04:33 +0300

    add web.api layer

---
+++ MedTech.Web.Api/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+using MedTech.Application.Mapping;
+
+namespace MedTech.Web.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //configure automapping in application layer
+            AutoMapperConfiguration.Configure();
+
+        }
+    }
+}
